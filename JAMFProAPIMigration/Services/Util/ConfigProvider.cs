155c64125983967a2d03cdcc2da0f046d38a7a7c
mattm <mtt.mortensen527@gmail.com>
2025-07-30 12:03:10 -0600

    A class that can be a source of true for api and token manager

---
+++ JAMFProAPIMigration/Services/Util/ConfigProvider.cs
@@ -0,0 +1,13 @@
+﻿namespace JAMFProAPIMigration.Services.Util
+{
+    public record ConfigProvider
+    {
+        private static readonly string JAMF_URL = Environment.GetEnvironmentVariable("JAMF_URL");
+        private static readonly string CLIENT_ID = Environment.GetEnvironmentVariable("JAMF_CLIENT_ID");
+        private static readonly string CLIENT_SECRET = Environment.GetEnvironmentVariable("JAMF_CLIENT_SECRET");
+
+        public static string GetJAMFURL() => JAMF_URL;
+        public static string GetCLIENT_ID() => CLIENT_ID;
+        public static string GetCLIENT_SECRET() => CLIENT_SECRET;
+    }
+}
