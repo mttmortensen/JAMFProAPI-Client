6725551be184b08e8f16b51a831cd0c5a40a4ba2
mattm <mtt.mortensen527@gmail.com>
2025-08-01 14:51:36 -0600

    First interface/service to break up API manager.

---
+++ JAMFProAPIMigration/Interfaces/IComputerService.cs
@@ -0,0 +1,13 @@
+﻿namespace JAMFProAPIMigration.Interfaces
+{
+    public interface IComputerService
+    {
+        Task<string> GetComputerIdByName(string computer);
+        Task<string> GetManagementIdByComputerId(string computerId);
+
+        // These truples will need to be models sometime soon
+        Task<List<(string computerId, string computerName)>> GetAllComputers();
+        Task<List<(string computerId, string managementId)>> GetAllManagementIds();
+        
+    }
+}
