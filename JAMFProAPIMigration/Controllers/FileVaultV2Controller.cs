a1d19bdda742b7fdd82da7a0da1843198300a595
mattm <mtt.mortensen527@gmail.com>
2025-08-01 14:18:10 -0600

    Using the DI here with APIManager to make sure the service is registering at runtime

--- JAMFProAPIMigration/Controllers/FileVaultV2Controller.cs
+++ JAMFProAPIMigration/Controllers/FileVaultV2Controller.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JAMFProAPIMigration.Services.Core;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JAMFProAPIMigration.Controllers
 {
+    [ApiController]
+    [Route("api/{controller}")]
     public class FileVaultV2Controller : ControllerBase
     {
-
+        private readonly ApiManager _manager;
+        public FileVaultV2Controller(ApiManager manager)
+        {
+            _manager = manager;
+        }
     }
 }
