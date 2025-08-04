801e9cd7cfda76a9cbc2fd8b10eefd9617cb7ce9
mattm <mtt.mortensen527@gmail.com>
2025-08-04 12:21:09 -0600

    Basic skeleton of the service layer.

---
+++ JAMFProAPIMigration/Services/Core/JamfHttpClient.cs
@@ -0,0 +1,25 @@
+﻿using JAMFProAPIMigration.Interfaces;
+
+namespace JAMFProAPIMigration.Services.Core
+{
+    public class JamfHttpClient : IJamfHttpClient
+    {
+        private readonly HttpClient _client;
+
+        public JamfHttpClient(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public Task<T> GetAsync<T>(string endpoint) 
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TResponse> PostAsync<TRequest, TResponse>(string endpoint, TRequest payload)
+        {
+            // TODO: serialize payload + call _client.PostAsync + deserialize
+            throw new NotImplementedException();
+        }
+    }
+}
