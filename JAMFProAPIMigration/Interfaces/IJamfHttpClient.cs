976daacfb719c17db93336e28fc60c6549ffcea0
mattm <mtt.mortensen527@gmail.com>
2025-08-04 12:20:37 -0600

    comments for understanding on the method header

--- JAMFProAPIMigration/Interfaces/IJamfHttpClient.cs
+++ JAMFProAPIMigration/Interfaces/IJamfHttpClient.cs
@@ -3,7 +3,13 @@
     public interface IJamfHttpClient
     {
         Task<T> GetAsync<T>(string endpoint);
-        Task<T> PostAsync<TRequest, TResponse>(string endpoint, TRequest payload);
+
+        // We are adding a type to work with
+        // And telling what type we're waning back 
+        // This is done with PostAsync<T,T> 
+        // TRquest is the request type we're sending in
+        // And TResponse is the type we expect to come out. 
+        Task<TResponse> PostAsync<TRequest, TResponse>(string endpoint, TRequest payload);
 
     }
 }
