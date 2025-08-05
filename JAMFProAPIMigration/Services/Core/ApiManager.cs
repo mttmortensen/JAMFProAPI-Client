a7a66a15575b4a5f9d03225a5419c2000a528f03
mattm <mtt.mortensen527@gmail.com>
2025-08-05 09:57:58 -0600

    removing methods from ApiManager to being on transition to IJamfHttpClient

--- JAMFProAPIMigration/Services/Core/ApiManager.cs
+++ JAMFProAPIMigration/Services/Core/ApiManager.cs
@@ -8,26 +8,6 @@ namespace JAMFProAPIMigration.Services.Core
     public class ApiManager 
     {
 
-        public static async Task<HttpClient> CreateHttpClientAsync()
-        {
-            var token = await TokenManager.GetTokenAsync();
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            return client;
-        }
-
-        public static HttpRequestMessage CreateRequest(HttpMethod method, string endpoint, string contentType = "application/json")
-        {
-
-            return new HttpRequestMessage
-            {
-                Method = method,
-                RequestUri = new Uri($"{ConfigProvider.GetJAMFURL()}{endpoint}"),
-                Headers = { { "accept", contentType } }
-            };
-        }
-
     }
 }
 
