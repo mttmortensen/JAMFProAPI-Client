d3810ed8b8aa876370ea3a27479834f6a57cfda0
mattm <mtt.mortensen527@gmail.com>
2025-08-05 09:47:10 -0600

    Adding logic for PostAsync. Including comments for understanding.

--- JAMFProAPIMigration/Services/Core/JamfHttpClient.cs
+++ JAMFProAPIMigration/Services/Core/JamfHttpClient.cs
@@ -1,6 +1,8 @@
 ﻿using JAMFProAPIMigration.Interfaces;
 using JAMFProAPIMigration.Services.Util;
 using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
 
 namespace JAMFProAPIMigration.Services.Core
 {
@@ -20,16 +22,50 @@ namespace JAMFProAPIMigration.Services.Core
             throw new NotImplementedException();
         }
 
-        public Task<TResponse> PostAsync<TRequest, TResponse>(string endpoint, TRequest payload)
+        public async Task<TResponse> PostAsync<TRequest, TResponse>(string endpoint, TRequest payload)
         {
 
             // --- 1. Grab a fresh token each call ----
-            var token = _tokenManager.GetTokenAsync();
+            var token = await _tokenManager.GetTokenAsync();
 
-            // TODO: serialize payload + call _client.PostAsync + deserialize
-            throw new NotImplementedException();
+            // --- 2. Build the request skeleton ---
+            var request = CreateRequest(HttpMethod.Get, endpoint);
+
+            // --- 3. Attach the JSON body ---
+            //  A. Turn the payload object into a JSON string: 
+            var jsonString = JsonSerializer.Serialize(payload);
+
+            //  B. Wrap that string in an HttpContent object, and proper headers: 
+            // StringContent is HttpContent object.
+            request.Content = new StringContent
+                (
+                    jsonString,
+                    Encoding.UTF8,
+                    "application/json"
+                );
+
+            // --- 4. Adding the Bearer token header ---
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            // --- 5. Send and Parse ----
+            var response = await _client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            // Streams are easier to load into memory since it's not 
+            // one giant block being thrown in. It comes in steadily as bytes.
+            // The next line will send make it into object we can use in the service layer
+            var stream = await response.Content.ReadAsStreamAsync();
+
+            // This is going to return whatever JAMF has setup for this return. 
+            // Helps it make more generic of a response vs something specific. 
+            return await JsonSerializer.DeserializeAsync<TResponse>(stream);
+            
         }
 
+
+        // Making this private due to what the IJamfHttpClient is standing for 
+        // Its only providing a contract for CRUD operations. 
+        // Creating the Http Request doesn't fall under that interface although it is still needed to be made. 
         private HttpRequestMessage CreateRequest(HttpMethod method, string endpoint, string contentType = "application/json")
         {
             var request = new HttpRequestMessage(method, endpoint);
