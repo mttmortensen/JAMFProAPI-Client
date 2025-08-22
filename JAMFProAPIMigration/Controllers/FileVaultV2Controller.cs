b2d3986fd981006fc297690a40dcfafb78a44809
mattm <mtt.mortensen527@gmail.com>
2025-08-22 11:13:15 -0600

    scaffolding on fv2 Controller.

--- JAMFProAPIMigration/Controllers/FileVaultV2Controller.cs
+++ JAMFProAPIMigration/Controllers/FileVaultV2Controller.cs
@@ -1,4 +1,5 @@
-﻿using JAMFProAPIMigration.Services.Core;
+﻿using JAMFProAPIMigration.Interfaces;
+using JAMFProAPIMigration.Services.Core;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JAMFProAPIMigration.Controllers
@@ -7,5 +8,17 @@ namespace JAMFProAPIMigration.Controllers
     [Route("api/{controller}")]
     public class FileVaultV2Controller : ControllerBase
     {
+        private readonly IFileVault2 _fv2Service;
+
+        public FileVaultV2Controller(IFileVault2 fv2Service)
+        {
+            _fv2Service = fv2Service;
+        }
+
+        [HttpGet("users")]
+        public IActionResult GetAllFV2Users()
+        {
+
+        }
     }
 }
