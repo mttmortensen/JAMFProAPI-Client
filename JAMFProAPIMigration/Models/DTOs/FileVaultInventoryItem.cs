4a03e469bb5eb1c2384d2c70407df709ff66157a
mattm <mtt.mortensen527@gmail.com>
2025-08-25 13:55:17 -0600

    adding new dto for fv2 inventory. Better to build out what I want vs using a raw JObject

---
+++ JAMFProAPIMigration/Models/DTOs/FileVaultInventoryItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace JAMFProAPIMigration.Models.DTOs
+{
+    public class FileVaultInventoryItem
+    {
+        public string? ComputerId { get; set; }
+        public string? Name { get; set; }
+        public string? PersonalRecvoeryKey { get; set; }
+        public string? IndividualKeyStatus { get; set; }
+    }
+}
